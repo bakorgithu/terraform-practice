f952d760fa5c8d791686f5e903984d0162b99137
bakor <bakorivoeyong237@gmail.com>
2025-01-19 02:56:29 +0100

    updated code

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 # Defining the EC2 instance
 
 resource "aws_instance" "bakor_ec2" {
-  ami = "ami-0df8c184d5f6ae949"
-  instance_type = "t3.micro"
-  key_name = "bakor_test"
+  ami = var.ami_id
+  instance_type = var.instance_type
+  key_name =var.key_name
   security_groups = [aws_security_group.allow_ssh.name]
-
+iam_instance_profile = var.instance_profile_name
   tags = {
-    Name = "bakor"
+    Name = var.instance_name
   }
 }
---
+++ vars.tf
@@ -0,0 +1,49 @@
+## variables
+
+variable "ami_id" {
+type = string
+default = "ami-0df8c184d5f6ae949"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"  
+}
+
+variable "instance_name" {
+    type = string
+    default = "bakor"
+}
+
+variable "region" {
+    type = string
+    default = "us-east-1"
+}
+
+variable "key_name" {
+    type = string
+}
+
+variable "role_name" {
+    type = string
+}
+
+variable "policy_name" {
+    type = string
+}
+
+variable "instance_profile_name" {
+    type = string
+}
+
+variable "aws_iam_role_policy" {
+    type = string
+}
+
+variable "assume_role_policy" {
+    type = string
+}
+
+variable "policy_arn" {
+  type = string
+}
