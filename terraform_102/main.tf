8f51174976786101200297353dc2c0d5c4a2866b
bakor <bakorivoeyong237@gmail.com>
2025-01-21 17:34:31 +0100

    updated code

--- terraform_102/main.tf
+++ terraform_102/main.tf
@@ -1,8 +1,25 @@
 ## Module
 
 module "ec2_module" {
-  source = "./terraform_102/module/ec2"
-  instance_type = "t3.micro"
-  key_name = "ivo-test"
-  instance_name = "Ivo"
+    source         = "./module/ec2"
+    instance_type  = var.instance_type
+    key_name       = var.key_name
+    instance_name  = var.instance_name
+    policy_arn = var.policy_arn
+    policy_name = var.policy_name
+    role_name = var.role_name
+    aws_iam_role_policy = var.aws_iam_role_policy
+    instance_profile_name = var.instance_profile_name
+    assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
